d995daa11273a732166f1f050ca4fb8bd1e4be71
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-30 10:44:20 +0200

    Create rastreo_exts.py

---
+++ rastreo_exts.py
@@ -0,0 +1,16 @@
+import os
+from VALID import ns
+
+filetype = raw_input("Inroduce extension: ")
+
+count = 0
+
+for (nombredir, dirs, ficheros) in os.walk('.'):
+	for nombrefichero in ficheros:
+		if nombrefichero.endswith(filetype):
+			print(nombrefichero)
+			count = count+1
+			
+print("")			
+print (count, 'archivos encontrados')
+			
