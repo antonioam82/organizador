cd71ae76c7ff6ddfddcd28fda269c5049b8986c1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-30 11:01:49 +0200

    Update rastreo_exts.py

--- rastreo_exts.py
+++ rastreo_exts.py
@@ -1,16 +1,16 @@
 import os
 from VALID import ns
-
+#print(os.getcwd())
 while True:
-	filetype = raw_input("Inroduce extension: ")
+	filetype = input("Inroduce extension: ")
 	count = 0
 	for (nombredir, dirs, ficheros) in os.walk('.'):
 	    for nombrefichero in ficheros:
 			if nombrefichero.endswith(filetype):
-				print(nombrefichero)
+				print(os.path.abspath(nombrefichero))
 				count = count+1
 	print("")
 	print (count, 'archivos encontrados')
-	conti = ns(input("\n¿Desea continuar?(n/s): ")
+	conti = ns(input("¿Desea continuar(n/s)?: ")
 	if conti == ("n"):
 		break
