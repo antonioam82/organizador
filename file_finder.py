7d7e9990e529b73d06ddffb22fe947ef5582ea3b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-25 14:22:29 +0200

    Update file_finder.py

--- file_finder.py
+++ file_finder.py
@@ -47,7 +47,7 @@ while True:
 	finded = False
 	change_dir()
 	dir_base = os.getcwd()
-	fichero_requerido = input("Inroduce término de busqueda: ")
+	fichero_requerido = input("Inroduce archivo a buscar o término de busqueda: ")
 	print("BUSCANDO...\n")
 	for root, folders, files in os.walk(dir_base):
 	    for file in files:
