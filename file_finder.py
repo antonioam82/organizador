ee8b38747481fd5bae25a196f0666ee78e0ebd0a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-31 16:05:38 +0200

    Update file_finder.py

--- file_finder.py
+++ file_finder.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
 from VALID import ns
 
 def AB():
     while True:
-	op = input("Introduje aquí su opción:")
-	if op == "A" or op == "B":
-	    return op
-	    break
-	else:
-	    print("Introduce A o B según su opción.")
+        op = input("Introduje aquí su opción:")
+        if op == "A" or op == "B":
+            return op
+            break
+        else:
+            print("Introduce A o B según su opción.")
 			
 def change_dir():
     while True:
-	dire = input("Introduzca directorio base: ")
-	if os.path.isdir(dire):
-	    os.chdir(dire)
-	    break
-	else:
-	    print("ERROR, DIRECTORIO NO VÁLIDO")
+        dire = input("Introduzca directorio base: ")
+        if os.path.isdir(dire):
+            os.chdir(dire)
+            break
+        else:
+            print("ERROR, DIRECTORIO NO VÁLIDO")
 	
 print("\n-----------------------------FILE FINDER------------------------------\n")		
 
@@ -33,34 +32,36 @@ while True:
     opc = AB()
 	
     if opc == 'A':
-	change_dir()
-	filetype = raw_input("Introduce extension: ")
-	print("BUSCANDO...")
-	for (nombredir, dirs, ficheros) in os.walk('.'):
-	    for nombrefichero in ficheros:
-		if nombrefichero.endswith(filetype):
-			count+=1
-			print('{}-'.format(count)+os.path.abspath(nombrefichero))
-			
-	print('\n{} ARCHIVOS ENCONTRADOS.'.format(count))
+        change_dir()
+        filetype = input("Introduce extension: ")
+        print("BUSCANDO...")
+        for (nombredir, dirs, ficheros) in os.walk('.'):
+            for nombrefichero in ficheros:
+                if nombrefichero.endswith(filetype):
+                    count+=1
+                    print('{}-'.format(count)+os.path.abspath(nombrefichero))
+                    
+        print('\n{} ARCHIVOS ENCONTRADOS.'.format(count))
 					
     elif opc == "B":
-	finded = False
-	change_dir()
+        finded = False
+        change_dir()
 	#dir_base = os.getcwd()
-	fichero_requerido = input("Inroduce archivo a buscar o término de busqueda: ")
-	print("BUSCANDO...\n")
-	for root, folders, files in os.walk('.'):
-	    for file in files:
-		if fichero_requerido in file: #file == fichero_requerido
-		    count+=1
-		    finded = True
-		    print('{}-Encontrado '.format(count)+file+' en '+os.path.join(root, file)+"\n")
-	if finded == False:
-	    print("No se encontró el archivo",fichero_requerido)
-	else:
-	    print("\n{} ARCHIVOS ENCONTRADOS.".format(count))
-					
+        fichero_requerido = input("Inroduce archivo a buscar o término de busqueda: ")
+        print("BUSCANDO...\n")
+        for root, folders, files in os.walk('.'):
+            for file in files:
+                if fichero_requerido in file: #file == fichero_requerido
+                    count+=1
+                    finded = True
+                    print('{}-Encontrado '.format(count)+file+' en '+os.path.join(root, file)+"\n")
+        if finded == False:
+            print("No se encontró el archivo",fichero_requerido)
+        else:
+            print("\n{} ARCHIVOS ENCONTRADOS.".format(count))
+            
     conti = ns(input("¿Continuar(n/s)?: "))
     if conti == "n":
-	break
+        break
+
+
